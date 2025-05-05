d16af3d4086df0802782a1b68bf09ebb7e6e87a6
Antonio Lucian Popa <89997382+Antonio-Lucian-Popa@users.noreply.github.com>
2025-05-05 15:02:34 +0300

    Delete webApp/factura_app directory

--- webApp/factura_app/procesare/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProcesareConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'procesare'
--- webApp/factura_app/procesare/widgets.py
+++
@@ -1,5 +0,0 @@
-# procesare/widgets.py
-from django.forms.widgets import FileInput
-
-class MultiFileInput(FileInput):
-    allow_multiple_selected = True
